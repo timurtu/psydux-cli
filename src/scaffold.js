a33c0e0d929c4b31e6ce9dde4215ddca789cf2f3
Timur Uzel <timurt.uzel@gmail.com>
2016-08-31 16:57:17 -0400

    yellow is friendlier

--- src/scaffold.js
+++ src/scaffold.js
@@ -15,7 +15,7 @@ const args = process.argv.slice(2)
 function checkArgs() {
   
   if (!args[0] || args[1]) {
-    log('red', 'Invalid number of arguments. Usage: psydux (name-of-app)')
+    log('yellow', 'Invalid number of arguments. Usage: psydux (name-of-app)')
   } else {
     return true
   }
