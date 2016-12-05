115835b834a523422a6360462e651dc3eb745e07
Timur Uzel <timur@Timurs-MacBook-Pro.local>
2016-12-05 01:28:00 -0500

    better error check and npm start

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,4 +1,4 @@
 
 ## Build from Source
 1. `npm install` to install dependencies
-1. `gulp watch` to build and watch for changes
+1. `npm start` to build and watch for changes
--- src/index.js
+++ src/index.js
@@ -7,5 +7,4 @@ const openAsync = Promise.promisify(require('fs').open)
 
 openAsync('.psydux', 'r')
   .then(() => require('./serve'))
-  .catch(e => e.message.trim().endsWith('.psydux\'') ?
-    require('./scaffold') : log('red', e))
+  .catch(e => e.code === 'ENOENT' ? require('./scaffold') : log('red', e))
